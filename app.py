f25584fce2051ca573433a0ff8c769e81f3ee8c6
Lifessons <105765857+swastikmaiti@users.noreply.github.com>
2024-05-10 07:14:47 +0000

    updated app

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def doc_processing(uploaded_pdf,var1):
     reader = PdfReader(uploaded_pdf)
     context_list = parese_doc(reader,first_section,ignore_after)
     index = create_embedding(context_list)
-    return {state_var:[context_list,index]}
+    return {input_box: gr.Textbox(value="Ask a question", visible=True),
+            state_var:[context_list,index]}
 
 def response_generator(text,var1):
     context_list,index = var1
@@ -112,17 +113,23 @@ def response_generator(text,var1):
         yield {output_box:output,state_var:[context_list,index]}  
     del generator
 
+def submit():
+    return {input_box: gr.Textbox(visible=True)}
+
 with gr.Blocks() as demo:
     
     state_var = gr.State([])
 
     with gr.Row():
         upload_button = gr.UploadButton("📁 Upload PDF", file_types=[".pdf"])
-    upload_button.upload(doc_processing,[upload_button,state_var],state_var,queue=False,show_progress=True,trigger_mode="once")
+    error_box = gr.Textbox(label="Error", visible=False)
 
-    input_box = gr.Textbox(autoscroll=True)
+    input_box = gr.Textbox(autoscroll=True,visible=False)
     output_box = gr.Textbox(autoscroll=True,max_lines=30)
     gr.Interface(fn=response_generator, inputs=[input_box,state_var], outputs=[output_box,state_var],delete_cache=(20,10))
+
+    upload_button.upload(doc_processing,inputs=[upload_button,state_var],outputs=[input_box,state_var],queue=False,show_progress=True,trigger_mode="once")
+    upload_button.upload(submit,None,input_box)
     
 demo.queue()
 demo.launch(share=True)
